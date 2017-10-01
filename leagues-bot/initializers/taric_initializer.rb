95181d6f09ff878d045ef92fd206b6acf848e6bb
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-10-01 13:40:20 -0500

    ignore taric_initializer

--- leagues-bot/initializers/taric_initializer.rb
+++ leagues-bot/initializers/taric_initializer.rb
@@ -6,7 +6,7 @@ require 'typhoeus/adapters/faraday'
 
 # Config API Key for Taric
 Taric.configure! do |config|
-  config.api_key = 'RGAPI-0020053d-293a-4240-be86-d688a3dbdcd6'
+  config.api_key = '<RIOT_api_key'>
   config.adapter = :typhoeus # default is Faraday.default_adapter
 end
 
